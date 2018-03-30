87c7b53e579e3cfcdf439d0db98ddf3f6492eeca
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2018-03-30 14:51:15 -0400

    Securing Chat box

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -2,9 +2,11 @@ from django.views.generic import TemplateView
 from service_info import settings
 from rest_framework.authtoken.models import Token
 from services.models import Provider
-import json
-
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+import json
+import hmac
+import hashlib
 
 
 class LandingPageView(LoginRequiredMixin, TemplateView):
@@ -19,11 +21,19 @@ class LandingPageView(LoginRequiredMixin, TemplateView):
 
         host = request.META['HTTP_HOST'].split(':')[0]
         SITE_CONFIG = getattr(settings, 'SITE_CONFIG', {})
-        
+
         site_settings = SITE_CONFIG.get('all', {})
         for k in SITE_CONFIG.keys():
             if k in host:
                 site_settings = SITE_CONFIG[k]
+                if site_settings.get("CHAT_ENABLED", False):
+                    digest = hmac.new(
+                        # secret key (keep safe!)
+                        site_settings['INTERCOM_SECRET'].encode('utf8'),
+                        request.user.email.encode('utf8'),  # user's email address
+                        digestmod=hashlib.sha256  # hash function
+                    ).hexdigest()
+                    site_settings['CHAT_DIGEST'] = str(digest)
                 break
 
         context = super(LandingPageView, self).get_context_data(**kwargs)
@@ -64,12 +74,11 @@ class LandingPageView(LoginRequiredMixin, TemplateView):
 
         context['REGION'] = 'false'
 
-        if hasattr(request,'region'):
+        if hasattr(request, 'region'):
             context['REGION'] = json.dumps({
                 'id': request.region.id,
                 'name': request.region.name,
                 'languages_available': request.region.languages_available,
             })
 
-            
         return context
