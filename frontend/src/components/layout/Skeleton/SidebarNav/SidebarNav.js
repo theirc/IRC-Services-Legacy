dd48ab53fee5e472466096808768ec1b53972d13
Juan Bressan <juan.bressan@rescue.org>
2019-07-01 21:07:57 -0300

    sidebar styles finished

--- frontend/src/components/layout/Skeleton/SidebarNav/SidebarNav.js
+++ frontend/src/components/layout/Skeleton/SidebarNav/SidebarNav.js
@@ -19,27 +19,27 @@ const SidebarNav = props => {
 				<Button variant='outline-secondary' size='sm' onClick={onClick}>x</Button>
 			</div>
 			<ul>
-				<li>
+				<li className={props.location.pathname.includes('services') && 'active'}>
 					<Link to='/services'><img src={process.env.PUBLIC_URL + '/static/assets/Skeleton/SidebarNav/nav-services.png'} />Services</Link>
 				</li>
 				<li>
 					<a target='_blank' href='https://www.refugee.info/'><img src={process.env.PUBLIC_URL + '/static/assets/Skeleton/SidebarNav/nav-service-map.png'} />Service map</a>
 				</li>
 				<hr /> {/* Separator */}
-				<li>
+				<li className={props.location.pathname.includes('providers') && 'active'}>
 					<Link to='/providers'><img src={process.env.PUBLIC_URL + '/static/assets/Skeleton/SidebarNav/nav-providers.png'} />Service providers</Link>
 				</li>
-				<li>
+				<li className={props.location.pathname.includes('regions') && 'active'}>
 					<Link to='/regions'><img src={process.env.PUBLIC_URL + '/static/assets/Skeleton/SidebarNav/nav-regions.png'} />Regions</Link>
 				</li>
-				<li>
+				<li className={props.location.pathname.includes('service-categories') && 'active'}>
 					<Link to='service-categories'><img src={process.env.PUBLIC_URL + '/static/assets/Skeleton/SidebarNav/nav-service-categories.png'} />Service categories</Link>
 				</li>
-				<li>
+				<li className={props.location.pathname.includes('provider-types') && 'active'}>
 					<Link to='provider-types'><img src={process.env.PUBLIC_URL + '/static/assets/Skeleton/SidebarNav/nav-provider-types.png'} />Provider types</Link>
 				</li>
 				<hr /> {/* Separator */}
-				<li>
+				<li className={props.location.pathname.includes('settings') && 'active'}>
 					<Link to='/settings'><img src={process.env.PUBLIC_URL + '/static/assets/Skeleton/SidebarNav/nav-settings.png'} />Settings</Link>
 				</li>
 			</ul>
