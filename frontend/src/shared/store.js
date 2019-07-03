be486dc1e9f185c67c43ac9c3cc2e6c93904a778
Juan Bressan <juan.bressan@rescue.org>
2019-07-03 15:02:59 -0300

    StorePersistor redesign commented out

--- frontend/src/shared/store.js
+++ frontend/src/shared/store.js
@@ -31,6 +31,124 @@ const reducers = {
 	router: connectRouter(history)
 };
 
-const store = createStore(combineReducers({ ...reducers }), initialState, enhancers);
+// saves state to local storage
+const persist = state => {
+	try {
+		localStorage.setItem('state', JSON.stringify(state));
+	} catch(e) {
+		console.log(e);
+	}
+}
+
+// loads state from local storage
+const load = () => {
+	try {
+		const serializedState = localStorage.getItem('state');
+
+		if(serializedState) {
+			let state = JSON.parse(serializedState);
+			state.router.location.pathname = window.location.pathname; // update router if url changed
+			return state;
+		}
+		
+		return undefined;
+	} catch(e) {
+		console.log(e);
+
+		return undefined;
+	}
+}
+
+const persistedState = load();
+const store = createStore(combineReducers({ ...reducers }), persistedState || initialState, enhancers);
+store.subscribe(() => persist(store.getState()));
 
 export default store;
+
+
+///////////////////////////////////////////////
+// StorePersistor class redesign
+/*
+export const history = createBrowserHistory();
+
+const reducers = {
+	login: loginReducers,
+	skeleton: skeletonReducers,
+	router: connectRouter(history)
+};
+
+const initialState = {
+	login: {
+		csrfToken: null
+	},
+	skeleton: {
+		sidebarnav: {
+			isOpen: true
+		}
+	},
+	user: null
+};
+
+// binding redux devtools extension and routerMiddleware(history) to dispatch history actions
+const enhancers = window.__REDUX_DEVTOOLS_EXTENSION__ ?
+	compose(applyMiddleware(routerMiddleware(history)), window.__REDUX_DEVTOOLS_EXTENSION__()) :
+	compose(applyMiddleware(routerMiddleware(history)));
+
+class PersistedStore {
+	constructor(reducers, initialState, enhancers, storeName) {
+		this.name = storeName;
+		const persistedState = this.load(); // overrides initial state
+		this.store = createStore(combineReducers({ ...reducers }), persistedState || initialState, enhancers);
+		this.store.subscribe(() => this.persist());
+	}
+
+	get name() {
+		return this.name;
+	}
+
+	set name(newName) {
+		this.name = newName;
+	}
+
+	get store() {
+		return this.store;
+	}	
+
+	set store(s) {
+		this.store = s;
+	}	
+
+	// saves state to local storage
+	persist() {
+		const state = this.store.getState();
+		try {
+			localStorage.setItem(this.name, JSON.stringify(state));
+		} catch(e) {
+			console.log(e);
+		}
+	}
+
+	// loads state from local storage
+	load() {
+		try {
+			const serializedState = localStorage.getItem(this.name);
+
+			if(serializedState) {
+				let state = JSON.parse(serializedState);
+				state.router.location.pathname = window.location.pathname; // update router if url changed
+				return state;
+			}
+			
+			return undefined;
+		} catch(e) {
+			console.log(e);
+
+			return undefined;
+		}
+	}
+}
+
+const ps = new PersistedStore(reducers, initialState, enhancers, 'app-state');
+
+export default ps.store;
+*/
