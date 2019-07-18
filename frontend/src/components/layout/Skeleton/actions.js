e86fd220e067ec705887956916e522029fd68896
Juan Bressan <juan.bressan@rescue.org>
2019-07-18 19:22:57 -0300

    action accidentally removed added back

--- frontend/src/components/layout/Skeleton/actions.js
+++ frontend/src/components/layout/Skeleton/actions.js
@@ -2,11 +2,12 @@ import { createAction } from 'redux-actions';
 
 const actionTypes = {
 	setDarkMode: 'SET_DARK_MODE',
-	setSidebarNavOpen: 'SET_SIDEBARNAV_OPEN',
 	setResultsPerPage: 'SET_RESULTS_PER_PAGE',
+	setSidebarNavOpen: 'SET_SIDEBARNAV_OPEN',
 }
 export default {
 	types: actionTypes,
 	setDarkMode: createAction(actionTypes.setDarkMode),
 	setResultsPerPage: createAction(actionTypes.setResultsPerPage),
+	setSidebarNavOpen: createAction(actionTypes.setSidebarNavOpen),
 };
