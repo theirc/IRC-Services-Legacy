d4d0012e3992691af4004d932e64eb631be65d7f
nciampechini <nahuel.ciampechini@thinkersys.com>
2019-07-30 15:59:59 -0300

    sp-1279:removing intercom from cuentanos settings

--- service_info/settings.py
+++ service_info/settings.py
@@ -528,7 +528,7 @@ INTERNAL_IPS = [
 
 SITE_CONFIG = {
     "cuentanos.org": {
-        "CHAT_ENABLED": True,
+        "CHAT_ENABLED": False,
         "CHAT_APP_ID": 'uchonpxg',
         "INTERCOM_SECRET": os.environ.get('CN_INTERCOM_SECRET', '')
     },
