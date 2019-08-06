f3579ec7dccb4e2b1b8fe5bc69528dbc32607d6d
nciampechini <nahuel.ciampechini@thinkersys.com>
2019-08-06 14:05:26 -0300

    Providers section tests

--- frontend/src/scenes/Providers/Providers.test.js
+++ frontend/src/scenes/Providers/Providers.test.js
@@ -1,69 +1,79 @@
 import React, { Suspense } from "react";
 import { shallow, mount, render } from "enzyme";
-import { Provider } from 'react-redux';
-import  Providers from "./Providers";
-import { prop } from '@uirouter/core';
 import  ListView from "./views/ListView/ListView";
 import  EditView from "./views/EditView/EditView"
-//import store from '../.././shared/store';
-import configureStore from 'redux-mock-store';
-import { create } from "react-test-renderer";
-//import { render } from '@testing-library/react'
-import expectationResultFactory from "jest-jasmine2/build/expectationResultFactory";
-import { findByTestAtrrr, testStore } from '../../.././Utils'
-import { isRegExp } from "util";
-import TestRenderer from 'react-test-renderer';
+import store from '../../shared/store';
+import { Provider } from 'react-redux';
+import Edit from '../../components/views/Edit/Edit'
 
+const mockedProps={
+    match:{
+        params:{
+            id: 'create'
+        }
+    }
+}
 
-describe('Providers tests: ', () => {
-  const mockedFunction = jest.fn();
-  const mockedFunctionForList = jest.fn();
-  const initialProps = {
+const mockedEmptyProps={
+    match:{
+        params:{
+            id: 'someProp'
+        }
+    }
+}
+
+const mockedFunction = jest.fn();
+const mockedPropsForEdit={
     history:{
-      goBack: () => { mockedFunction();
-      }
+        goBack: () => { mockedFunction();
+        }
     }
-  }
-  const propsForList = {
-    history:{},
-    rowEvents:{
-      onClick: () =>{mockedFunctionForList();}
-    }
-  }
-  const listcomponent = shallow(<ListView {...propsForList} />);
-  const edit = shallow(<EditView {...initialProps} onClick={mockedFunction}/>);
-  const w = edit.find('button');
-  const listToShallow = listcomponent.find('List');
-  
-  
-  it('List view renders ok:', () => {
-    const title = listcomponent.find('h2');
-    const titleText = title.text();
-    expect(listcomponent.exists()).toBe(true);
-    expect(titleText).toBe('PROVIDERS');
-  })
+}
 
-  it('Checks list view structure:', () => {
-    expect(listcomponent.find('.ListView').exists()).toBe(true);
-    expect(listcomponent.find('h2').exists()).toBe(true);
-    expect(listcomponent.find('List').exists()).toBe(true);
-  })
+const viewEdit = mount(<Provider store={store}>
+    <Suspense fallback=''>
+        <Edit {...mockedPropsForEdit} onClick={mockedFunction}/>
+    </Suspense>
+</Provider>);
 
-  it('Edit view renders ok:', () => {
-    expect(edit.exists()).toBe(true);
-    expect(w.text()).toBe('< Back');
-  })
 
-  it('Checks edit view structure:', () => {
-    const editss = edit.find('Edit');
-    const h2s = edit.find('h2');
-    expect(editss.length).toBe(6);
-    expect(h2s.length).toBe(1);
-  })
+const editWithProps = mount(<Provider store={store}>
+		<Suspense fallback=''>
+            <EditView {...mockedProps}/>
+		</Suspense>
+</Provider>);
 
-  it('Back button:', () => {
-    w.simulate('click');
-    expect(mockedFunction).toHaveBeenCalled();
-  })
+const editWithoutProps = mount(<Provider store={store}>
+    <Suspense fallback=''>
+        <EditView {...mockedEmptyProps}/>
+    </Suspense>
+</Provider>);
 
+
+const list = shallow(<ListView />);
+
+describe('Providers Tests: ', () => {
+    it('Edit view render with match.params.id equeal to create prop:', () => {
+        expect(editWithProps.find('Form').exists()).toBe(true);
+    })
+
+    it('Edit view render with match.params.id not equeal to create prop:', () => {
+        expect(editWithoutProps.find('EditView').exists()).toBe(true);
+    })
+    it('Back button on Edit View: ', () =>{
+        const backButton = viewEdit.find('button');
+        backButton.simulate('click');
+        expect(mockedFunction).toHaveBeenCalled();
+    })
+    it('List view renders ok:', () => {
+        const title = list.find('h2');
+        const titleText = title.text();
+        expect(list.exists()).toBe(true);
+        expect(titleText).toBe('list.title');
+    })
+    it('Checks list view structure:', () => {
+        expect(list.find('.ListView').exists()).toBe(true);
+        expect(list.find('h2').exists()).toBe(true);
+        expect(list.find('Connect(List)').exists()).toBe(true);
+    })
 })
